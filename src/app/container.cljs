b690558be2a5fd95a13863700c629c0dc05f4108
jiyinyiyong <jiyinyiyong@gmail.com>
2020-05-13 00:50:56 +0800

    added a simple example

--- src/app/container.cljs
+++ src/app/container.cljs
@@ -5,7 +5,17 @@
 (defcomp
  comp-container
  (store)
- (let [states (:states store), cursor []]
+ (let [states (:states store)
+       cursor []
+       state (or (:data states) {:count 0})]
    (container
     {}
-    (text {:text "DEMO", :style {:fill (hslx 240 80 80), :font-family "Josefin Sans"}}))))
+    (rect {:position [40 80]
+           :size [80 80]
+           :fill (hslx 200 60 90)
+           :on {:pointertap (fn [e d!] (d! cursor (update state :count inc)))}})
+    (text {:text (str "Clicked " (:count state) " times.")
+           :position [20 20]
+           :style {:fill (hslx 240 80 80)
+                   :font-family "Helvetica Neue"
+                   :font-weight 300}}))))
